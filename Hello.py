96daf73cf4a339157d4637b410288ca3a7dd1af8
Mizukichi404 <nekotora6925@icloud.com>
2022-05-17 17:13:03 +0900

    2022/5/17 17:11 change history

--- Hello.py
+++ Hello.py
@@ -1,2 +1,28 @@
-a = 1
-print (a)
+import numpy as np
+
+a = np.array([1,2,3])
+
+a
+
+print(a)
+
+type(a)
+
+a.shape
+
+b = np.array([[1, 2, 3], [4, 5, 6]])
+
+b
+b.shape
+
+c1 = np.array([0, 1, 2, 3, 4, 5])
+c1
+
+c2 = c1.reshape((2, 3))
+c2
+
+c3 = c2.ravel()
+c3
+
+c4 = c2.flatten() #copy を返す
+c4
