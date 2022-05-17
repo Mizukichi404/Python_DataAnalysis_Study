72493f479119a810b81a062c7441a72f3a2fb6f2
Mizukichi404 <nekotora6925@icloud.com>
2022-05-17 17:50:31 +0900

    2022/5/17 17:50 Commit history

--- Hello.py
+++ Hello.py
@@ -1,6 +1,9 @@
 import numpy as np
+import sys
 
-a = np.array([1,2,3])
+sys.maxsize
+
+a = np.array([1, 2, 3])
 
 a
 
@@ -26,3 +29,22 @@ c3
 
 c4 = c2.flatten() #copy を返す
 c4
+
+a.dtype
+
+d = np.array([1, 2], dtype=np.int16)
+d.dtype
+d.astype(np.float16)
+
+a
+a[0]
+a[1:]
+a[-1]
+
+b
+b[0]
+b[1, 0]
+b[:, 2]
+b[1, :]
+b[0, 1:]
+b[:, 0:2]
