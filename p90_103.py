09e49965378c7a1e7ee43305815e411ffe8a3c13
Mizukichi404 <nekotora6925@icloud.com>
2022-05-19 22:52:17 +0900

    2022/05/19 Commit History

--- p90_103.py
+++ p90_103.py
@@ -48,3 +48,50 @@ b[:, 2]
 b[1, :]
 b[0, 1:]
 b[:, 0:2]
+
+a
+a[2] = 4
+a
+
+b
+b[1, 2] = 7
+b
+b[:, 2] = 8
+b
+
+a1 = a
+a1
+
+a1[1] = 5
+a1
+
+a
+
+a2 = a.copy()
+a2
+
+a2[0] = 6
+a2
+
+a
+
+c2
+c2[0, 0] = 0
+c4 = c2.flatten()
+c3[0] = 6
+c4[1] = 7
+
+c3
+c4
+c2
+py_list1 = [0, 1]
+py_list2 = py_list1[:]
+py_list2[0] = 2
+print(py_list1)
+print(py_list2)
+
+np_array1 = np.array([0, 1])
+np_array2 = np_array1[:]
+np_array2[0] = 2
+print(np_array1)
+print(np_array2)
