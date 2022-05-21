6d18b2a1b019d1f4f74d1799c78ec04a72c020df
Mizukichi404 <nekotora6925@icloud.com>
2022-05-22 01:21:24 +0900

    Commit

--- MySQL/connection.py
+++ MySQL/connection.py
@@ -6,11 +6,21 @@ connection = MySQLdb.connect(
     host='localhost',
     user='root',
     passwd='test',
-    db='python_db')
+    db='python_db',
+    #テーブル内部で日本語を扱うための追加
+    charset='utf8'
+)
 cursor = connection.cursor()
 
 # ここに実行したいコードを入力します
 cursor.execute("SELECT * FROM m_users")
+
+#fetchall()で全件取り出し
+rows = cursor.fetchall()
+#fetchone()で1件ずつ取り出し
+rows = cursor.fetchone()
+#テーブルのデータがemptyのため、'NoneType'object is not iterableのエラーがでる
+print(rows)
 
 # 保存を実行
 connection.commit()
